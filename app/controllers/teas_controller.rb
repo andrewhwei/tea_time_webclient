25586ff2c924721d8502b9e023e272f793989364
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-31 20:01:17 -0700

    Added in api key authorizations

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -24,7 +24,7 @@ class TeasController < ApplicationController
 
   def update
     tea = Tea.find(params[:id])
-    tea.update(name: params[:name], origin: params[:origin], weight: params[:weight], in_stock: params[:in_stock])
+    tea.update(id: params[:id], name: params[:name], origin: params[:origin], weight: params[:weight], in_stock: params[:in_stock])
     redirect_to "/teas/#{tea.id}"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'teas', to: 'teas#index'
   get 'teas/new', to: 'teas#new'
   get 'teas/:id/edit', to: 'teas#edit'
-  patch 'teas/:id', to: 'teas#update'
   get 'teas/:id', to: 'teas#show'
+  patch 'teas/:id', to: 'teas#update'
   post 'teas', to: 'teas#create'
   delete 'teas/:id', to: 'teas#destroy'
 
